1a86e4a5362cc2d20596f952017c52b85389eb33
Nelzio <nelziositoe@gmail.com>
2019-10-03 11:58:09 +0200

    Ateracoes no char

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -2,26 +2,22 @@
   <q-page padding>
     <!-- content -->
     <div class="q-pa-md row justify-center">
-    <div style="width: 100%;">
-    	<div v-for="message in messages" :key="message.id">
-      <q-chat-message
-      	v-if="message.user == 1"
-        name="me"
-        :text="[message.message]"
-        sent
-      />
-      <q-chat-message
-      v-else
-        name="Outro"
-        :text="[message.message]"
-      />
+      <div style="width: 100%;">
+        <div v-for="message in messages" :key="message.id">
+        <q-chat-message
+          v-if="message.user == 1"
+          name="me"
+          :text="[message.message]"
+          sent
+        />
+        <q-chat-message
+        v-else
+          name="Outro"
+          :text="[message.message]"
+        />
+        </div>
       </div>
     </div>
-  </div>
-  
-    
-    
-    
 
     <q-footer class="text-white q-pl-md q-pb-md q-pr-md bg-white">
     	<q-form
@@ -67,44 +63,7 @@ export default {
       this.message = null
       
     },
-    // send_message () {
-    // 	const self = this
-    // 	const request = Mixin.methods.client_rest(this.$store.state.tokenLog)
-    // 	const user = this.$store.state.authUser.idUser
-    // 	const chatId = this.$store.state.chatId
-    // 	const url = "http://127.0.0.1:8005/api/chats/"
-    // 	request.post(url, {
-    // 		message: this.message,
-    // 		chatId: chatId,
-    // 		receiver: 2,
-    // 		user: user
-    // 	}).then((response)=>{
-    // 		this.messages.push(response.data)
-    // 		console.log(response.data)
-    // 		self.$store.commit('SET_CHAT_ID', response.data.chatId)
-    //     this.message = null
-    //   })
-    // },
-    // get_messages () {
-    // 	const self = this
-    // 	const request = Mixin.methods.client_rest(this.$store.state.tokenLog)
-    // 	const user = this.$store.state.authUser.idUser
-    // 	const chatId = this.$store.state.chatId
-    // 	let items = []
-    // 	const url = "http://127.0.0.1:8005/api/chats/"
-    // 	request.get(url, {
-    // 		params: {
-    // 		    chatId: chatId
-    // 		    }
-    // 		  }).then((response)=>{
-	  //   		  	const endIndex = this.messages.length + 10
-	  //   		  	items.push(response.data.slice(this.messages.length, endIndex))
-	  //   		  	this.messages = this.messages.concat(items[0])
-    //   })
-    // },
-    //=============================================
-    //============================================
-    //==============================================
+
     sendMessage() {
       const user = 1
       const chatId = 1
@@ -119,9 +78,6 @@ export default {
   },
   mounted() {
   	this.page_title()
-    //inicializarFirebase()
-  	//this.get_messages()
-  	//setInterval(this.get_messages, 3000)
     let self = this;
     const itemsRef = firebaseInit.database().ref('messages');
     itemsRef.on('value', snapshot => {
